fdcac46d0eda93c2f54720d92aa0925915eabffc
刘鹏程 <liupengcheng@dajiagame.com>
2016-07-22 08:31:56 +0800

    no message

---
+++ Assets/TestPosScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+    /// <summary>
+    /// 描述：
+    /// author： 
+    /// </summary>
+	[AddComponentMenu("DajiaGame/Px/ TestPosScript ")]
+    public class TestPosScript : MonoBehaviour
+    {
+        public static TestPosScript instance;
+
+        public Vector3 DirVector3;
+        public float left;
+        public float right;
+
+        void Awake()
+        {
+            instance = this;
+        }
+    }
