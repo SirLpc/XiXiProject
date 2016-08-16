d06be725c2491c887e1ab293920e1122cdaf912d
sirlpc <sirlpc123@gmail.com>
2016-08-16 08:07:51 +0800

    no message

--- Assets/BossCtr.cs
+++ Assets/BossCtr.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
-using System.Collections.Generic;
-using ADBannerView = UnityEngine.iOS.ADBannerView;
 using Random = UnityEngine.Random;
 
 
